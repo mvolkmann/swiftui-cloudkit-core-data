87d5ec6bbfe30718e23446efb1c53e56b2649acd
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-08 08:13:35 -0500

    Initial Commit

---
+++ swiftui-cloudkit-core-data/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-cloudkit-core-data
+//
+//  Created by Mark Volkmann on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-cloudkit-core-data/swiftui_cloudkit_core_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_cloudkit_core_dataApp.swift
+//  swiftui-cloudkit-core-data
+//
+//  Created by Mark Volkmann on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_cloudkit_core_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
