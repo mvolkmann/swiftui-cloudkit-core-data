8d3dd4701074805530d2fa15c805b650412615b3
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-09 08:53:35 -0500

    polish

--- swiftui-cloudkit-core-data/AddSportView.swift
+++ swiftui-cloudkit-core-data/AddSportView.swift
@@ -7,24 +7,38 @@ struct AddSportView: View {
     @State private var name = ""
     
     var body: some View {
-        Form {
-            MyTextField(text: $name, placeholder: "name", label: "Sport Name")
-            Button("Save") {
-                let text = name.trimmingCharacters(in: .whitespaces)
-                if text.isEmpty { return }
-                
-                // Create an empty Sport object.
-                let sport = Sport(context: dbContext)
-                // Set the attributes.
-                sport.name = text
-                // Update Core Data.
-                do {
-                    try dbContext.save()
-                } catch {
-                    print("Error saving sport \(text)")
-                }
-                dismiss()
+        NavigationView {
+            Form {
+                MyTextField(
+                    text: $name,
+                    placeholder: "name",
+                    label: "Sport Name"
+                )
             }
+            .navigationBarItems(
+                leading: Button("Cancel", action: { dismiss() }),
+                trailing: Button("Save", action: save)
+            )
         }
+    }
+    
+    func save() {
+        let text = name.trimmingCharacters(in: .whitespaces)
+        if text.isEmpty { return }
+        
+        // Create an empty Sport object.
+        let sport = Sport(context: dbContext)
+    
+        // Set the attributes.
+        sport.name = text
+    
+        // Update Core Data.
+        do {
+            try dbContext.save()
+        } catch {
+            print("Error saving sport \(text)")
+        }
+    
+        dismiss()
     }
 }
--- swiftui-cloudkit-core-data/AddTeamView.swift
+++ swiftui-cloudkit-core-data/AddTeamView.swift
@@ -8,25 +8,39 @@ struct AddTeamView: View {
     let sport: Sport
     
     var body: some View {
-        Form {
-            MyTextField(text: $name, placeholder: "name", label: "Team Name")
-            Button("Save") {
-                let text = name.trimmingCharacters(in: .whitespaces)
-                if text.isEmpty { return }
-                
-                // Create an empty Team object.
-                let team = Team(context: dbContext)
-                // Set the attributes.
-                team.name = text
-                team.sport = sport
-                // Update Core Data.
-                do {
-                    try dbContext.save()
-                } catch {
-                    print("Error saving team \(text)")
-                }
-                dismiss()
+        NavigationView {
+            Form {
+                MyTextField(
+                    text: $name,
+                    placeholder: "name",
+                    label: "Team Name"
+                )
             }
+            .navigationBarItems(
+                leading: Button("Cancel", action: { dismiss() }),
+                trailing: Button("Save", action: save)
+            )
         }
+    }
+    
+    func save() {
+        let text = name.trimmingCharacters(in: .whitespaces)
+        if text.isEmpty { return }
+        
+        // Create an empty Team object.
+        let team = Team(context: dbContext)
+        
+        // Set the attributes.
+        team.name = text
+        team.sport = sport
+        
+        // Update Core Data.
+        do {
+            try dbContext.save()
+        } catch {
+            print("Error saving team \(text)")
+        }
+        
+        dismiss()
     }
 }
